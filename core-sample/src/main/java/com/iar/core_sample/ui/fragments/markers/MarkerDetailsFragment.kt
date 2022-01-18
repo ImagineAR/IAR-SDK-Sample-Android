158429fce019226bedde5cd98f3ce1c8842b13d2
lihongACY <lihongguo@imaginear.com>
2022-01-18 16:02:31 -0500

    IAR-4908 Add screen showing marker detail Json data

--- core-sample/src/main/java/com/iar/core_sample/ui/fragments/markers/MarkerDetailsFragment.kt
+++ core-sample/src/main/java/com/iar/core_sample/ui/fragments/markers/MarkerDetailsFragment.kt
@@ -5,20 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import com.google.gson.GsonBuilder
 import com.iar.core_sample.R
+import com.iar.core_sample.databinding.FragmentMarkerDetailsBinding
+import com.iar.core_sample.databinding.RewardDetailsFragmentBinding
+import com.iar.core_sample.ui.fragments.rewards.RewardDetailsFragmentArgs
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MarkerDetailsFragment : Fragment() {
 
+    private lateinit var binding: FragmentMarkerDetailsBinding
+    private val args by navArgs<MarkerDetailsFragmentArgs>()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_marker_details, container, false)
-    }
+    ): View{
+        binding = FragmentMarkerDetailsBinding.inflate(inflater, container, false)
 
+        val marker = args.marker
+        val gson = GsonBuilder().setPrettyPrinting().create()
+         val prettyJsonString = gson.toJson(marker)
+        binding.markerData.text = prettyJsonString
+        return binding.root
+    }
 
 }
