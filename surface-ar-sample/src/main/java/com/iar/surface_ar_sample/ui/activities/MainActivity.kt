dad77ae70f2f682159fc2a0b0f828d3c746e43a2
lihongACY <lihong@imaginear.com>
2022-02-01 15:46:24 -0500

    IAR-4920 Implement write NFC functionality

--- surface-ar-sample/src/main/java/com/iar/surface_ar_sample/ui/activities/MainActivity.kt
+++ surface-ar-sample/src/main/java/com/iar/surface_ar_sample/ui/activities/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.iar.surface_ar_sample.ui.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.iar.nfc_sdk.NFCController
 import com.iar.surface_ar_sample.databinding.ActivityMainBinding
+import com.iar.surface_ar_sample.ui.fragments.nfc.NFCViewModel
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    val nfcViewModel by viewModels<NFCViewModel>()
+
+    private lateinit var nfcController: NFCController
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,5 +28,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        nfcController = NFCController(this)
+        nfcViewModel.setNfcController(nfcController)
+
     }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+
+        if (intent.action != null) {
+            nfcViewModel.setIntent(intent)
+
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        nfcController.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        nfcController.onPause()
+    }
+
+
 }
--- surface-ar-sample/src/main/java/com/iar/surface_ar_sample/ui/fragments/nfc/WriteNFCFragment.kt
+++ surface-ar-sample/src/main/java/com/iar/surface_ar_sample/ui/fragments/nfc/WriteNFCFragment.kt
@@ -6,18 +6,58 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.iar.surface_ar_sample.R
+import com.iar.nfc_sdk.NFCController
+import com.iar.surface_ar_sample.databinding.FragmentWriteNfcBinding
+import com.iar.surface_ar_sample.ui.activities.MainActivity
+import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 
+@AndroidEntryPoint
 class WriteNFCFragment : Fragment() {
 
+    private lateinit var binding: FragmentWriteNfcBinding
+    private var nfcController: NFCController? = null
+    var markerId = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_write_nfc, container, false)
+    ): View {
+        binding = FragmentWriteNfcBinding.inflate(inflater, container, false)
+        val currentActivity = activity as? MainActivity
+
+        currentActivity?.nfcViewModel?.nfcController?.observe(viewLifecycleOwner) { controller ->
+            controller?.let {
+                nfcController = controller
+            }
+        }
+        binding.writeButton.setOnClickListener { view ->
+            nfcController?.let {
+                currentActivity?.nfcViewModel?.startNfc(it)
+                markerId = binding.markerInput.editText?.text.toString()
+                currentActivity?.nfcViewModel?.closeKeyBoard(view, currentActivity)
+            }
+
+        }
+
+        currentActivity?.nfcViewModel?.currentIntent?.observe(viewLifecycleOwner) { intent ->
+            nfcController?.let {
+
+                val writeMessage = currentActivity.nfcViewModel.writeNfc(markerId, it, intent)
+
+                binding.writeMessage.text = writeMessage
+            }
+        }
+
+        binding.randomizeButton.setOnClickListener {
+            nfcController?.let {
+                currentActivity?.nfcViewModel?.startNfc(it)
+                markerId = UUID.randomUUID().toString()
+                binding.markerInput.editText?.setText(markerId)
+            }
+        }
+
+        return binding.root
     }
-
-
 
 }
