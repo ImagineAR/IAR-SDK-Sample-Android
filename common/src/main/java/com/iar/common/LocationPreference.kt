04c76efa47599e2731297bee1c51eb1fefb0ea04
lihongACY <lihong@imaginear.com>
2022-02-24 17:06:58 -0500

    IAR-5341 Fix the bug “Invalid coordinates crashes the app”

--- common/src/main/java/com/iar/common/LocationPreference.kt
+++ common/src/main/java/com/iar/common/LocationPreference.kt
@@ -2,8 +2,10 @@ package com.iar.common
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
+import androidx.core.content.ContextCompat
 import androidx.preference.Preference
 import androidx.preference.PreferenceViewHolder
 
@@ -20,7 +22,12 @@ class LocationPreference @JvmOverloads constructor(
         val button = holder.findViewById(R.id.center_button) as Button
 
         if (isEnabled) {
-            editLocation.setText(sharedPreferences?.getString(context.getString(R.string.location_coordinates_key), "") ?: "")
+            editLocation.setText(
+                sharedPreferences?.getString(
+                    context.getString(R.string.location_coordinates_key),
+                    ""
+                ) ?: ""
+            )
             button.isEnabled = true
             editLocation.isFocusable = true
             editLocation.isFocusableInTouchMode = true
@@ -34,7 +41,6 @@ class LocationPreference @JvmOverloads constructor(
 
             button.isClickable = true
             button.setOnClickListener {
-
                 if (button.text == "Center of NA") {
                     editLocation.setText(context.getString(R.string.center_coordinates))
                 }
@@ -43,10 +49,20 @@ class LocationPreference @JvmOverloads constructor(
 
                 button.text = context.getString(R.string.center_of_na)
 
-                with(sharedPreferences?.edit()) {
-                    this?.putString(context.getString(R.string.location_coordinates_key), text)
-                    this?.apply()
+                if (text.isNotEmpty() && checkValidCoordinates(text)) {
+                    with(sharedPreferences?.edit()) {
+                        this?.putString(context.getString(R.string.location_coordinates_key), text)
+                        this?.apply()
+                    }
+
+                } else {
+                    button.text = context.getString(R.string.apply)
+                    Utils.showToastMessage("Please enter valid coordinates.", context)
                 }
+
+                //Hide keyboard
+                val imm = ContextCompat.getSystemService(context, InputMethodManager::class.java)
+                imm?.hideSoftInputFromWindow(it.windowToken, 0)
             }
 
         } else {
@@ -55,5 +71,21 @@ class LocationPreference @JvmOverloads constructor(
             editLocation.isFocusable = false
             editLocation.isFocusableInTouchMode = false
         }
+    }
+
+    //Add this function here temporarily, should add this in SDK
+    private fun checkValidCoordinates(coordinates: String): Boolean{
+        val positionString = coordinates.split("[\\s,]+".toRegex()).toTypedArray()
+
+        if (positionString.size > 1) {
+            try {
+                positionString[0].toDouble()
+                positionString[1].toDouble()
+                return true
+            } catch (e: NumberFormatException) {
+                return false
+            }
+        }
+        return false
     }
 }
